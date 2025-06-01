ecb89fd9502b257a4078162c4a5357c3a5ce26d6
juanspartan177 <juanjosilva25@gmail.com>
2025-06-01 18:43:23 -0400

    Actualización de código y nuevos archivos de configuración

--- config.py
+++ config.py
@@ -1,20 +1,19 @@
 # ferremas_api/config.py
-import os
+from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
 from pathlib import Path
-from pydantic_settings import BaseSettings  # Cambio aquí
+
 
 load_dotenv(dotenv_path=Path(__file__).parent / ".env")
 
 class Settings(BaseSettings):
-    FERREMAS_DB_API_TOKEN: str = os.getenv("FERREMAS_DB_API_TOKEN", "SaGrP9ojGS39hU9ljqbXxQ==")
-    FERREMAS_DB_API_URL: str = os.getenv("FERREMAS_DB_API_URL", "https://ea2p2assets-production.up.railway.app")
-    SECRET_KEY: str = os.getenv("SECRET_KEY", "your-super-secret-and-very-long-key")
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    FERREMAS_DB_API_TOKEN: str
+    FERREMAS_DB_API_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     class Config:
-        env_file = ".env"
+        env_file = ".env"  
 
 settings = Settings()
-
