8836390e60d2ba671974fbc7a6182fed07c14734
juanspartan177 <juanjosilva25@gmail.com>
2025-06-01 18:56:59 -0400

    Actualización de código y nuevos archivos de configuración

--- config.py
+++ config.py
@@ -1,10 +1,6 @@
 # ferremas_api/config.py
+# config.py
 from pydantic_settings import BaseSettings
-from dotenv import load_dotenv
-from pathlib import Path
-
-
-load_dotenv(dotenv_path=Path(__file__).parent / ".env")
 
 class Settings(BaseSettings):
     FERREMAS_DB_API_TOKEN: str
@@ -14,6 +10,6 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     class Config:
-        env_file = ".env"  
+        env_file = ".env"
 
 settings = Settings()
